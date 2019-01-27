df9e96e158ac3980eb805ec35d467c798b881b04
Harsh1204 <ashish12.1993gupta@gmail.com>
2019-01-27 15:57:51 -0500

    6commit

--- src/scripts/CashTest.java
+++ src/scripts/CashTest.java
@@ -9,13 +9,12 @@ public class CashTest extends BaseTest {
 	public void cashtest() {
 
 
-<<<<<<< HEAD
+
 		System.out.println("ashish");
 	
-=======
-		
+
 		System.out.println("kkr");
->>>>>>> 0352f8049e14bdfb8e52f037861046c061f48154
+
 		
 
 	}
